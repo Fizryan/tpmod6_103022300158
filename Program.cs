c11ddf1ca7ffff4683d0459aee4dd52fe15c6339
Fizryan <52873306+Fizryan@users.noreply.github.com>
2025-03-24 01:13:15 +0700

    Implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmod6_103022300158;
+
+SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - HafizryandinHM");
+
+video.printVideoDetails();
+
+video.IncreasePlayCount(5000);
+video.printVideoDetails();
+
+video.IncreasePlayCount(10000000);
+video.printVideoDetails();
+
+for (int i = 0; i < 5; i++)
+{
+    video.IncreasePlayCount(int.MaxValue);
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace tpmod6_103022300158
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+    
+        public SayaTubeVideo(string videoTitle)
+        {
+            Debug.Assert(videoTitle != null, "Judul video tidak boleh null");
+            Debug.Assert(videoTitle.Length <= 100, "Judul video tidak boleh lebih dari 100 karakter");
+
+            id = GenerateRandomId();
+            title = videoTitle;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int countToAdd)
+        {
+            Debug.Assert(countToAdd <= 10000000, "Input penambahan play count tidak boleh melebihi 10.000.000");
+
+            try
+            {
+                checked
+                {
+                    playCount += countToAdd;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("ERROR: Terjadi overflow pada play count!");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine($"Videp ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+
+        private int GenerateRandomId()
+        {
+            Random random = new Random();
+            return random.Next(100000, 999999);
+        }
+    }
+}
